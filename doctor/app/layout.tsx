289369d977ef1497fae6b3d0ed9ab07457f2fe0d
Hà Minh Nhật <nhatha@bravesoft.com.vn>
2024-04-04 18:50:46 +0700

    add font

--- doctor/app/layout.tsx
+++ doctor/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { JosefinSans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import React from "react";
-const inter = Inter({ subsets: ["latin"] });
+const inter = JosefinSans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- doctor/components/Header/MobileHeader.tsx
+++ doctor/components/Header/MobileHeader.tsx
@@ -4,26 +4,36 @@ import user from "@/public/icon/user-white.svg"
 import HeartLine from "@/public/icon/heart-line-white.svg"
 import cartLine from "@/public/icon/cart-bag-line-white.svg"
 import homeLine from "@/public/icon/home-line-white.svg"
-import 'swiper/css/pagination';
+import Shield from "@/public/icon/Shield-Star.svg"
+import React from 'react';
+
 export default function MobileHeader() {
 
     return (
         <>
             <div className="mobile-header">
                 <div className={`mobile-banner`}>
-
+                    <div
+                        className="w-[430px] h-[30px] px-5 py-2.5 bg-blue-900 justify-start items-center gap-2.5 inline-flex">
+                        <div className="justify-center items-center gap-1.5 flex">
+                            <div className="w-3.5 h-3.5 relative"><Image alt={`menu`} src={Shield} width={32} height={32}/></div>
+                            <div className="text-center">
+                                <span className="text-orange-400 text-xs font-semibold font-['Josefin Sans']">100% </span>
+                                <span className="text-orange-400 text-xs font-normal font-['Josefin Sans']">guaranteed</span>
+                                <span className="text-white text-xs font-normal font-['Josefin Sans']"> medical certificates</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className={`mobile-header`}></div>
             </div>
 
             <div
-                className={`w-lvw h-20 px-5 py-6 bg-[#223680] justify-between items-center inline-flex fixed bottom-0 mobile-fixed`}>
-                <a href="#">
-                    <Image alt={`asdasdasd`} src={menu} width={32} height={32}></Image>
-                </a>
-                <a href="#"><Image alt={`menu`} src={user} width={32} height={32}></Image></a>
-                <a href="#"><Image alt={`user`} src={HeartLine} width={32} height={32}></Image></a>
-                <a href="#"><Image alt={`like`} src={cartLine} width={32} height={32}></Image></a>
+                className={`w-dvw h-20 px-5 py-6 bg-[#223680] justify-between items-center inline-flex fixed bottom-0`}>
+                <a href="#"><Image alt={`menu`} src={menu} width={32} height={32}></Image></a>
+                <a href="#"><Image alt={`user`} src={user} width={32} height={32}></Image></a>
+                <a href="#"><Image alt={`like`} src={HeartLine} width={32} height={32}></Image></a>
+                <a href="#"><Image alt={`cart`} src={cartLine} width={32} height={32}></Image></a>
                 <a href="#"><Image src={homeLine} alt={`home`} width={32} height={32}></Image></a>
             </div>
         </>
