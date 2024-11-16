59759f96bbf61ad60c99549278ed09801fc2854b
Hà Minh Nhật <131330599+bsv-nhatha@users.noreply.github.com>
2024-11-16 23:39:11 +0700

    delete any :))

--- doctor/components/Footer/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './_Footer.scss';
-import {MobileFooter} from "@/components/Footer/MobileFooter";
-export type FooterProps = {}
-
-export default function Footer({}: FooterProps) {
-    return (<MobileFooter />);
-}
--- doctor/lib/Logger.ts
+++
@@ -1,9 +0,0 @@
-export default function logger(object: unknown, comment?: string): void {
-    console.log(
-        '%c ============== INFO LOG \n',
-        'color: #22D3EE',
-        `${typeof window !== 'undefined' && window?.location.pathname}\n`,
-        `=== ${comment ?? ''}\n`,
-        object
-    );
-}
